5ab3c2733dee0e7e77212cba7d66ebbca7973327
Petr Zalizko <pzalizko@gmail.com>
2024-05-15 12:32:09 +0300

    Add project files.

---
+++ ASP_NET_Core_MVC_My_Tests/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_NET_Core_MVC_My_Tests.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
